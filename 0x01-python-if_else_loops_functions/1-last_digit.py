e2814109ff71ca18c476ce189b106d67bca8dd3b
Emediong Francis <emediongfrancis@gmail.com>
2021-09-08 16:30:59 -0700

    added akpatre number.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if akpatre > 5:
     print("Last digit of", number, "is", akpatre, "and is greater than 5")
 elif akpatre == 0:
     print("Last digit of", number, "is", akpatre, "and is 0")
-else:
+elif akpatre < 6 and not 0:
     print("Last digit of", number, "is", akpatre, "and is less than 6 and not 0")
