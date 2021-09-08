f39c2eca9980ca0b00fe0002a43f428ca80bc797
Emediong Francis <emediongfrancis@gmail.com>
2021-09-08 16:34:48 -0700

    added akpatre number.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-akpatre = number % 10
+akpatre = abs(number) % 10
 if akpatre > 5:
     print("Last digit of", number, "is", akpatre, "and is greater than 5")
 elif akpatre == 0:
