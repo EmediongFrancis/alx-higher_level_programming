c28c3c47435858ad68b7d7a05fbb1513881ccf67
Emediong Francis <emediongfrancis@gmail.com>
2021-09-13 15:03:02 -0700

    no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,3 @@
-
 #!/usr/bin/python3
-
-
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+def no_c(my_string):
+    return ''.join(c for c in my_string if c not in 'cC')
