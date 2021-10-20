af0c5d52efa87d6a95c4ab5f19fc7fa10cd29d92
Emediong Francis <emediongfrancis@gmail.com>
2021-10-20 16:21:08 -0700

    added mylist.

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""MyList class"""
+
+
+class MyList(list):
+    """subset of list"""
+    def __init__(self):
+        """inits obj"""
+        super().__init__()
+
+        def print_sorted(self):
+            """prints sorted list"""
+            print(sorted(self))
