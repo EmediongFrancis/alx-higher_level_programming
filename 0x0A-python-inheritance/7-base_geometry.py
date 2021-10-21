d5fe7bc900cd2ac11d1f59fc59a22a600a2d0a70
Emediong Francis <emediongfrancis@gmail.com>
2021-10-21 06:52:50 -0700

    extended geo.

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Geometry class"""
+
+
+class BaseGeometry:
+    """class with area & integer_validatoe methods"""
+    def area(self):
+        """raises exception if called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validation(self, name value):
+        """validates value as int > 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
