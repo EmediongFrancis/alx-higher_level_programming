c7b5b0f96351fe2d6ab2dd747a804fe2d9f6713b
Emediong Francis <emediongfrancis@gmail.com>
2021-11-16 13:13:27 +0100

    create database if not exists.

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -3,5 +3,4 @@
 -- You are not allowed to use the SELECT or SHOW statements.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0; -- Create database.
-SHOW DATABASES;
 DROP DATABASE IF EXISTS hbtn_0c_0; -- Removes database.
