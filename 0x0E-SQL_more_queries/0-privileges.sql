eba66cae5cbde44d6cd9e6127d6d88613cf65a4b
Emediong Francis <emediongfrancis@gmail.com>
2021-11-17 18:51:17 +0100

    showed privileges.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
-SHOW GRANTS FOR `user_0d`_1 AND `user_0d_2`;
+SHOW GRANTS FOR `user_0d_1` AND `user_0d_2`;
