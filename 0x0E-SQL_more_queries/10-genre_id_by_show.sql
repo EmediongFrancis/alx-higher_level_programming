17cc078c30eb114d33488c65f81414d40dc6b211
Emediong Francis <emediongfrancis@gmail.com>
2021-11-18 00:48:43 +0100

    fixed styling indent.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
-SELECT tv_shows.title, tv_show_genres.id
+SELECT tv_shows.title, tv_show_genres.genre_id
     FROM tv_genres
     JOIN tv_show_genres
     JOIN tv_shows
