8c76c3d74633f318fb4672464044f6e73e04bb23
Emediong Francis <emediongfrancis@gmail.com>
2021-11-18 00:50:06 +0100

    fixed typos.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,6 +4,6 @@ SELECT tv_shows.title, tv_show_genres.genre_id
     FROM tv_genres
     JOIN tv_show_genres
     JOIN tv_shows
-    ON tv_shows.id = show.id
-    AND tv_genres.id = genre.id
+    ON tv_shows.id = show_id
+    AND tv_genres.id = genre_id
     ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
