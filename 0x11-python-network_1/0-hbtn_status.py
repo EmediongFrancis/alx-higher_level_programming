002838a850dfb620a38f887cb0169163ffa0c527
Emediong Francis <emediongfrancis@gmail.com>
2022-01-10 13:35:36 +0100

    Fetch hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,15 @@
 '''
     Script that fetches https://intranet.hbtn.io/status.
 '''
+
 import urllib.request
 
 if __name__ == "__main__":
-    fetchedUrl = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(fetchedUrl) as fu:
-        fur = fu.read()
+
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        res = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(fur)))
-        print("\t- content: {}".format(fur))
-        print("\t- utf8 content: {}".format(fur.decode("utf-8")))
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode("utf-8")))
